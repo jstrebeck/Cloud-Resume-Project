d235c3cedb90369c5081b9b487b119e7311521a4
jstrebeck <vba2ismlg@hotmail.com>
2021-05-25 11:44:13 -0700

    Add javascript hit counter

--- js/main.js
+++ js/main.js
@@ -89,4 +89,17 @@ jQuery(document).ready(function( $ ) {
   }
   ];
   var myDoughnut = new Chart(document.getElementById("illustrator").getContext("2d")).Doughnut(doughnutData);
+
+
+  const countEl = document.getElementById('count');
+
+updateVisitCount();
+
+function updateVisitCount() {
+	fetch('https://api.countapi.xyz/update/florin-popcom/codepen/?amount=1')
+	.then(res => res.json())
+	.then(res => {
+		countEl.innerHTML = res.value;
+	})
+}
 });
